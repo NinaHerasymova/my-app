1c5f2eec6ed435429f1787f0bc797a011a2088e4
Nina Herasymova <predvoditeldvoryanstva@gmail.com>
2020-06-07 16:01:16 +0300

    my profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader";
+import userPhoto from "../../../assets/images/user.png";
 
 const ProfileInfo = (props) => {
 
@@ -14,7 +15,7 @@ const ProfileInfo = (props) => {
         <img alt='wallpaper' src='https://www.w3schools.com/w3css/img_lights.jpg'/>
       </div>
       <div className={s.descriptionBlock}>
-        <img alt='avatar' src={props.profile.photos.large}/>
+        <img alt='avatar' src={props.profile.photos.large!== null ? props.profile.photos.large : userPhoto}/>
         <div className={s.personInform}>
           <span className={s.fullName}>{props.profile.fullName}</span>
           <p className={s.status}>{props.profile.aboutMe}</p>
